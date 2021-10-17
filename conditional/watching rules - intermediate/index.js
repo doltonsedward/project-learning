3d193a88a7eb00b573d1cfcb7dbeb3dd1bb1b5c0
D3NCyan <edwarddoltons@gmail.com>
2021-10-17 17:02:09 +0800

    change from html to pure js

--- conditional/watching rules - intermediate/index.js
+++ conditional/watching rules - intermediate/index.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wathing rules</title>
-</head>
-<body>
-    
-<script>
 const movies = [
     {
         title: 'Harry potter', 
@@ -54,6 +43,3 @@ function getAge(choices) {
 }
 
 movieApp()
-</script>
-</body>
-</html>
